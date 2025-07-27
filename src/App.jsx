a778709ce7cbce8906be4ffc94a33e35a90917d5
“Hordhik” <kondamanikant4@gmail.com>
2025-07-28 02:06:58 +0530

    Completed NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
 
+// Create placeholder components for each route
+const Home = () => <div>Home Page</div>;
+const Blogs = () => <div>Blogs Page</div>;
+const Events = () => <div>Events Page</div>;
+const About = () => <div>About Page</div>;
 
 function App() {
 
   return (
-    <>
       <NavBar />
-    </>
+      // <Route path="/" element={<Home />} />
+      // <Route path="/blogs" element={<Blogs />} />
+      // <Route path="/events" element={<Events />} />
+      // <Route path="/about" element={<About />} />
   )
 }
 
